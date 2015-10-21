9a22d8622834c80ebc60f7085a0daff1847b43d6
shannon mulloy & yelena nedelko <student@epicodus.com>
2015-10-21 16:48:47 -0700

    ability to add locations

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -12,4 +12,18 @@ class LocationsController < ApplicationController
   def new
     @location = Location.new
   end
+
+  def create
+    @location =Location.new(location_params)
+    if @location.save
+      redirect_to locations_path
+    else
+      render :new
+    end
+  end
+
+  private
+  def location_params
+    params.require(:location).permit(:name, :avatar)
+  end
 end
